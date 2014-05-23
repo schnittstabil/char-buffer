f11db24a7ccc2ab28fc7ef4e7b318b9d6f6df53e
Michael Mayer <michael@schnittstabil.de>
2014-05-24 00:41:02 +0200

    bugfix: coveralls path part2

--- src/build/resources/Gruntfile.js
+++ src/build/resources/Gruntfile.js
@@ -29,7 +29,7 @@ module.exports = function(grunt) {
           coverage: true,
           recursive: true,
           reporter: 'spec',
-          root: '.',
+          root: '..',
           mask: '**/*_test.js',
           require: ['test/inject'],
           reportFormats: ['cobertura', 'lcovonly'],
