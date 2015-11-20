4e40c66d5f3534ee1414b2cbd02c7d4d495f60d2
Michael Mayer <michael@schnittstabil.de>
2015-11-20 09:15:12 +0100

    tweak tests

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,7 +2,6 @@
 
 module.exports = function(grunt) {
   var browsers = grunt.file.readJSON('browsers.json'),
-      legacyBrowsers = grunt.file.readJSON('legacy-browsers.json'),
       branch = process.env.TRAVIS_BRANCH,
       isPull = process.env.TRAVIS_PULL_REQUEST !== 'false',
       semver = branch ? branch.match(/^v?\d+\.\d+.\d+/) : null,
@@ -23,49 +22,21 @@ module.exports = function(grunt) {
 
     },
     'saucelabs-mocha': {
-      es6: {
+      all: {
         options: {
           urls: [
-            'http://127.0.0.1:9999/tests.html',
+            'http://127.0.0.1:9999/tests_es6.html',
+            'http://127.0.0.1:9999/tests_amd.html',
+            'http://127.0.0.1:9999/tests_global.html',
           ],
           testname: testname,
-          build: build + ' es6',
+          build: build,
           'public': visiblity,
           'record-video': false,
           'record-screenshots': true,
           throttled: 1,
           browsers: browsers,
-          tags: ['es6'],
-        },
-      },
-      amd: {
-        options: {
-          urls: [
-            'http://127.0.0.1:9999/tests_amd.html',
-          ],
-          testname: testname,
-          build: build + ' amd',
-          'public': visiblity,
-          'record-video': false,
-          'record-screenshots': true,
-          throttled: 1,
-          browsers: legacyBrowsers,
-          tags: ['amd'],
-        },
-      },
-      global: {
-        options: {
-          urls: [
-            'http://127.0.0.1:9999/tests_global.html',
-          ],
-          testname: testname,
-          build: build + ' global',
-          'public': visiblity,
-          'record-video': false,
-          'record-screenshots': true,
-          throttled: 1,
-          browsers: legacyBrowsers,
-          tags: ['global'],
+          tags: [testname],
         },
       },
     },
@@ -77,10 +48,5 @@ module.exports = function(grunt) {
   }
 
   grunt.registerTask('default', ['connect', 'watch']);
-
   grunt.registerTask('test', ['connect', 'saucelabs-mocha']);
-
-  grunt.registerTask('test:es6', ['connect', 'saucelabs-mocha:es6']);
-  grunt.registerTask('test:global', ['connect', 'saucelabs-mocha:global']);
-  grunt.registerTask('test:amd', ['connect', 'saucelabs-mocha:amd']);
 };
